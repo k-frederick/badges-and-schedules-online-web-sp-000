1d01be1bfd8aa2e23ae307c85c6e5b1047c3e73a
Kristina Frederick <kristinafrederick@icloud.com>
2020-05-20 22:39:55 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,7 +17,7 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  batch_badge_creator.each do |attendee|
+  batch_badge_creator(attendees).each do |attendee|
     puts |attendee|
   end
 end
