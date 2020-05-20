e88e9caecbc8b359d5e0934226d51fad36812775
Kristina Frederick <kristinafrederick@icloud.com>
2020-05-20 22:07:26 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,6 +9,7 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees)
+  results = []
   attendees.each_with_index do |attendee, index|
     "Hello, #{attendee}! You'll be assigned to room #{index}."
   end
