d414b728125e1b91513b39c0c68fdd84e966e096
Kristina Frederick <kristinafrederick@icloud.com>
2020-05-20 21:06:25 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+def badge_maker(name = "Arel")
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.map do |attendee|
+    "Hello, my name is #{attendee}."
+  end
+end
+
+def assign_rooms(attendees)
+  
+end
+
+def printer(attendees)
+
+end
